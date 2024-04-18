4aebe360e4397c8fee91565e1caae1dfe938b58a
etabebe1 <etabebe1@gmail.com>
2024-04-18 14:47:47 -0400

    header completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { useNavigate, Link } from "react-router-dom";
 
 // Importing React Icon
 import { MdOutlineCloseFullscreen } from "react-icons/md";
+import { RiMenu3Line } from "react-icons/ri";
 
 function Header() {
   const menuItems = [
@@ -38,7 +39,7 @@ function Header() {
   const [listStyle, setListStyle] = useState(menuItems[0].name);
 
   const handleNavigation = (path, name) => {
-    alert(`Navigating to: ${name}`);
+    // alert(`Navigating to: ${name}`);
     setIsOpen(!isOpen);
     setListStyle(name);
 
@@ -50,34 +51,91 @@ function Header() {
     <header>
       <nav className="z-50">
         <div
-          className={`slider-component sm:hidden block w-[70vw] sm:w-full h-[100vh] sm:h-[64px] bg-colors-digital-gray-2 absolute ${
+          className={`slider-component sm:hidden block w-full sm:w-full  h-[100vh] sm:h-[64px]  bg-colors-digital-gray/25 absolute backdrop-blur-sm ${
             isOpen ? " left-0" : "-left-full"
-          } z-50 transition-all duration-300 pt-4`}
+          } z-50 transition-all duration-300`}
+          onClick={() => setIsOpen(false)}
         >
-          <button
-            className="close-btn absolute  bg-colors-deep-space-blue p-1 rounded-md hover:border border-colors-quantum-silver right-5 text-2xl"
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            <span>
-              <MdOutlineCloseFullscreen />
-            </span>
-          </button>
+          <div className="layer  pt-4 w-[70vw] bg-colors-digital-gray h-full">
+            <button
+              className="close-btn absolute  bg-colors-matrix-green p-1 rounded-md hover:border border-colors-quantum-silver right-5 text-2xl text-white"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <span>
+                <MdOutlineCloseFullscreen />
+              </span>
+            </button>
 
-          <ul className="link-container w-full h-full p-3 flex flex-col gap-3 pt-14 text-white font-semibold text-lg">
-            {menuItems.map((list, index) => (
-              <Link to={"#"} key={index + 1}>
-                <li
-                  className={`${
-                    list.name === listStyle &&
-                    "bg-colors-quantum-silver/50 rounded-sm"
-                  } p-1`}
-                  onClick={() => handleNavigation(list.path, list.name)}
-                >
-                  {list.name}
-                </li>
-              </Link>
-            ))}
-          </ul>
+            <ul className="link-container w-full h-full p-3 flex flex-col gap-8 pt-14 text-white font-semibold text-lg">
+              {menuItems.map((list, index) => (
+                <Link to={"#"} key={index + 1}>
+                  <li
+                    className={`shadow-lg ${
+                      list.name === listStyle &&
+                      "text-colors-matrix-green border-b-2 border-r-2 rounded-md border-colors-matrix-green transition-all duration-500"
+                    } p-1`}
+                    onClick={() => handleNavigation(list.path, list.name)}
+                  >
+                    {list.name}
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        {/* mobile version header */}
+        <div className="flex gap-4 shadow-lg sm:shadow-none sm:backdrop-blur-none fixed w-full h-16 bg-colors-digital-gray border-b-1 z-30 backdrop-blur-md">
+          <div className="max-w-4xl w-full flex flex-row sm:flex-row justify-between px-3 items-center mx-auto">
+            <button
+              className=" text-white text-2xl bg-colors-matrix-green rounded-md p-1 sm:hidden hover:border border-colors-quantum-silver"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <RiMenu3Line />
+            </button>
+
+            <Link to={"/"}>
+              <div className="logo-image-container flex gap-2 items-center my-2 group">
+                <img
+                  src={"/assets/EPhone.jpg"}
+                  alt=""
+                  className="w-12 h-1w-12 rounded-xl group-hover:scale-105 transition-all"
+                />
+                <h1 className="text-colors-matrix-green text-xl font-semibold group-hover:text-colors-matrix-green/90 transition-all">
+                  EPhone App
+                </h1>
+              </div>
+            </Link>
+
+            {/* nav link container */}
+            <div className="link-container hidden sm:block">
+              <ul className="space-x-2 md:space-x-5 lg:space-x-10 flex justify-center items-center">
+                {menuItems.map((list, index) => {
+                  return (
+                    <Link to={`${list.path}`} key={index}>
+                      <button className="text-lg">
+                        <span
+                          className={`flex flex-row items-center text-sm md:text-base hover:text-white/50 transition-all duration-300`}
+                        >
+                          <p
+                            className={`text-xl text-white shadow-md  px-3 py-0.5 ${
+                              list.name === listStyle &&
+                              "text-colors-matrix-green border-b-2 border-r-2 rounded-lg  border-colors-matrix-green transition-all duration-500"
+                            } `}
+                            onClick={() =>
+                              handleNavigation(list.path, list.name)
+                            }
+                          >
+                            {list.name}
+                          </p>
+                        </span>
+                      </button>
+                    </Link>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
         </div>
       </nav>
     </header>
